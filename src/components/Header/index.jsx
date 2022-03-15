eb311de4754deeabc17697c4e8ba8489b10dd40c
Pedro <pedro.souza@comporte.com.br>
2022-03-15 18:28:07 -0300

    fix: 🐛 adjust build deploy && add logo header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,7 @@ const Header = () => {
     return (
         <div className='header'>
             <Link to="/">
+                <img src='logo512.png' alt="Joke Norris" />
                 <h1>Joke Norris</h1>
             </Link>
         </div>
