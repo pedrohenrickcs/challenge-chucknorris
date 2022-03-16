9a7e064c36114af3599ada2ceb9c15f4128b2725
Pedro <pedro.souza@comporte.com.br>
2022-03-16 10:17:07 -0300

    fix: 🐛 adjust link footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,8 @@
-import { Link } from 'react-router-dom';
-
 const Footer = () => {
     return (
         <footer className='footer'>
             <p className='footer__text'>
-                Built with ❤ and React and deployed with Github Pages, by <Link to="https://github.com/pedrohenrickcs" className='footer__text'>@PedroHenricks</Link> - © 2022
+                Built with ❤ and React and deployed with Github Pages, by <a href="https://github.com/pedrohenrickcs" target="_blank" rel="noreferrer" className='footer__text'>@PedroHenricks</a> - © 2022
             </p>
         </footer>
      );
